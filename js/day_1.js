5308176f290b9fb8fe4341fc1cf82e9e9cc130aa
akirak302 <jason.kawasaki@gmail.com>
2016-04-04 20:51:54 -0700

    bump

--- js/day_1.js
+++ js/day_1.js
@@ -1,2 +1,4 @@
-var myFavoriteNumber;
-var notMyFavoriteNumber;
+var myFavoriteNumber= 7;
+var notMyFavoriteNumber= "25";
+
+console.log = My favorite number is: <myFavoriteNumber>, it's not: <notMyFavoriteNumber>;
